bedf64dbb38ff9cdfaf4b5d7763cb70336afc2bf
titan-spyer <mr.satya02@gmail.com>
2026-04-13 23:17:34 +0530

    solved: grouping

---
+++ LeetcodeSQL/LC1341.sql
@@ -0,0 +1,58 @@
+Input: 
+Movies table:
++-------------+--------------+
+| movie_id    |  title       |
++-------------+--------------+
+| 1           | Avengers     |
+| 2           | Frozen 2     |
+| 3           | Joker        |
++-------------+--------------+
+Users table:
++-------------+--------------+
+| user_id     |  name        |
++-------------+--------------+
+| 1           | Daniel       |
+| 2           | Monica       |
+| 3           | Maria        |
+| 4           | James        |
++-------------+--------------+
+MovieRating table:
++-------------+--------------+--------------+-------------+
+| movie_id    | user_id      | rating       | created_at  |
++-------------+--------------+--------------+-------------+
+| 1           | 1            | 3            | 2020-01-12  |
+| 1           | 2            | 4            | 2020-02-11  |
+| 1           | 3            | 2            | 2020-02-12  |
+| 1           | 4            | 1            | 2020-01-01  |
+| 2           | 1            | 5            | 2020-02-17  | 
+| 2           | 2            | 2            | 2020-02-01  | 
+| 2           | 3            | 2            | 2020-03-01  |
+| 3           | 1            | 3            | 2020-02-22  | 
+| 3           | 2            | 4            | 2020-02-25  | 
++-------------+--------------+--------------+-------------+
+Output: 
++--------------+
+| results      |
++--------------+
+| Daniel       |
+| Frozen 2     |
++--------------+
+# Write your MySQL query statement below
+
+(SELECT u.name AS results
+FROM Users u
+JOIN MovieRating mr ON u.user_id = mr.user_id 
+GROUP BY u.name
+ORDER BY COUNT(mr.movie_id) DESC, u.name ASC 
+LIMIT 1)
+
+UNION ALL
+
+
+(SELECT m.title AS results
+FROM Movies m
+JOIN MovieRating mr ON m.movie_id = mr.movie_id
+WHERE mr.created_at LIKE '2020-02%'
+GROUP BY m.title
+ORDER BY AVG(mr.rating) DESC, m.title ASC
+LIMIT 1);
